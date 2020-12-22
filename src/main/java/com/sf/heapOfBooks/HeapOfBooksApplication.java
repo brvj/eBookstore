54ecee4d3ed4b3108791de1d723b0022600399a2
Boris <Boris@Ryzen-DESKTOP>
2020-12-22 15:48:50 +0100

    Inicijalni komit

---
+++ src/main/java/com/sf/heapOfBooks/HeapOfBooksApplication.java
@@ -0,0 +1,13 @@
+package com.sf.heapOfBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeapOfBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeapOfBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sf/heapOfBooks/HeapOfBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sf.heapOfBooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeapOfBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
