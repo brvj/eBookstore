22b2c7e360be6d214bdf9623dfd8c1741bdc1bae
Boris <Boris@Ryzen-DESKTOP>
2021-01-07 16:55:49 +0100

    Ubacen
    sqlQuery za pocetne podatke web aplikacije.

--- src/main/java/com/sf/heapOfBooks/HeapOfBooksApplication.java
+++ src/main/java/com/sf/heapOfBooks/HeapOfBooksApplication.java
@@ -8,5 +8,4 @@ public class HeapOfBooksApplication {
 	public static void main(String[] args) {		
 		SpringApplication.run(HeapOfBooksApplication.class, args);
 	}
-
 }
---
+++ src/main/resources/db/startData.sql
@@ -0,0 +1,78 @@
+
+CREATE table users(
+	id int primary key auto_increment,
+    userName varchar(20),
+    userPassword varchar(30),
+    eMail varchar(30),
+    `name` varchar(20),
+    surname varchar(40),
+    dateOfBirth Date,
+    address varchar(50),
+    phoneNumber varchar(20),
+    registrationDateAndTime datetime,
+    userType varchar(15),
+    userBlocked varchar(6)
+);
+
+create table genres(
+	id int primary key auto_increment,
+    `name` varchar(40),
+    description varchar(300),
+    deleted varchar(6)
+);
+
+create table books(
+	id bigint primary key auto_increment,
+    `name` varchar(30),
+    publicher varchar(30),
+    authors varchar(100),
+    releaseDate Date,
+    `description` varchar(200),
+    imagePath varchar(30),
+    price float,
+    numberOfPages int,
+    bookType varchar(20),
+    letter varchar(20),
+    bookLanguage varchar(30),
+    avgRating float,
+    numberOfCopies int
+);
+
+create table bookGenre(
+	bookId bigint,
+    genreId int,
+    primary key(bookId, genreId),
+    foreign key(bookId) references books(id)
+		on delete cascade,
+	foreign key(genreId) references genres(id)
+		on delete cascade
+);
+
+insert into users(id,userName,userPassword,eMail,`name`,surname,dateOfBirth,address,phoneNumber, registrationDateAndTime,userType,userBlocked) 
+values(1,'brvj','sifra','jankovicb0230@gmail.com','Boris','Jankovic',current_date(),'Jovana Jovanovica Zmaja 3','0696354635','2020-01-12 10:10:10','Administrator','false');
+
+insert into genres(id,`name`,description,deleted) values (1,'Horror','A horror film is one that seeks to elicit fear in its audience for entertainment purposes.','false');
+insert into genres(id,`name`,description,deleted) values(2,'Comedy','Comedy is a literary genre and a type of dramatic work that is amusing and satirical in its tone, mostly having a cheerful ending.','false');
+
+insert into books(id ,`name` , publicher , authors , releaseDate , description , imagePath , price , numberOfPages , bookType , letter , bookLanguage , avgRating , numberOfCopies ) 
+values(1000000000001,'Dracula','Public domain','Bram Stoker','1897-05-26','The novel tells the story of Draculas attempt to move from Transylvania to England so that he may find new blood and spread the undead curse','dracula-cover.jpg',680,418,'Tvrdi','Latinica','Srpski',4.0,100);
+insert into books(id ,`name` , publicher , authors , releaseDate , description , imagePath , price , numberOfPages , bookType , letter , bookLanguage , avgRating , numberOfCopies ) 
+values(1000000000002,'Good Omens','Gollancz','Terry Pratchett,Neil Gaiman','1990-05-10','The book is a comedy about the birth of the son of Satan and the coming of the End Times.','220px-Goodomenscover.jpg',1000,288,'Tvrdi','Latinica','Srpski',4.24,50);
+
+insert into bookGenre(bookId,genreId) values(1000000000001,1);
+insert into bookGenre(bookId,genreId) values(1000000000002,1);
+insert into bookGenre(bookId,genreId) values(1000000000002,2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
